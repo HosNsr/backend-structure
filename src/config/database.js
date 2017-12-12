68d35b8078a8fcab56e01148787d98e06772edc1
Hosein Naseri <h.naseri1990@gmail.com>
2017-12-12 13:33:50 +0330

    check the LOCAL_DB or not to connect to the database

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,14 @@ const mongoOptions = {
 	useMongoClient: true,
 };
 
-const uri = `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`;
-mongoose.connect(uri, mongoOptions);
+if(process.env.LOCAL_DB) {
+	const uri = `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`;
+	mongoose.connect(uri, mongoOptions);
+}
+else {
+	const uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+	mongoose.connect(uri, mongoOptions);
+}
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
